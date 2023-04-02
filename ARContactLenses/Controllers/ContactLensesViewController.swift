d0d4291f856a4fdd11cbd58c6e79a9df178447ba
JamesThang996 <baokieuha@gmail.com>
2023-04-02 13:32:43 +0700

    fin

---
+++ ARContactLenses/Controllers/ContactLensesViewController.swift
@@ -0,0 +1,82 @@
+
+import UIKit
+import ARKit
+
+final class ContactLensesViewController: UIViewController {
+
+    /// Set renderer
+    private let sceneView = ARSCNView(frame: .zero)
+
+    /// Declare eye nodes
+    private var leftEyeNode: ImageNode?
+    private var rightEyeNode: ImageNode?
+
+    /// Specify ARConfiguration
+    private let faceTrackingConfiguration = ARFaceTrackingConfiguration()
+
+    override func loadView() {
+        view = sceneView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard ARFaceTrackingConfiguration.isSupported else { fatalError("A TrueDepth camera is required") }
+        /// Set sceneView delegate
+        sceneView.delegate = self
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sceneView.session.run(faceTrackingConfiguration)
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        sceneView.session.pause()
+    }
+}
+
+
+extension ContactLensesViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+
+        // Note: - You must compile this project with an iPhone with TrueDepth camera as target device, otherwise it will mark that sceneView has no `device` member
+
+        /// Validate anchor is an ARFaceAnchor instance
+        guard anchor is ARFaceAnchor,
+            let device = sceneView.device else { return nil }
+
+        /// Create node with face geometry
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let node = SCNNode(geometry: faceGeometry)
+
+        node.geometry?.firstMaterial?.colorBufferWriteMask = []
+
+        /// Create eye ImageNodes
+        rightEyeNode = ImageNode(width: 0.015, height: 0.015, image: #imageLiteral(resourceName: "Sharingan_3"))
+        leftEyeNode = ImageNode(width: 0.015, height: 0.015, image: #imageLiteral(resourceName: "Sharingan_1"))
+
+        /// Change the origin of the eye nodes
+        rightEyeNode?.pivot = SCNMatrix4MakeTranslation(0, 0, -0.01)
+        leftEyeNode?.pivot = SCNMatrix4MakeTranslation(0, 0, -0.01)
+
+        /// Add child nodes
+        rightEyeNode.flatMap { node.addChildNode($0) }
+        leftEyeNode.flatMap { node.addChildNode($0) }
+
+        return node
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        /// Get face anchor as ARFaceAnchor
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+            let faceGeometry = node.geometry as? ARSCNFaceGeometry else { return }
+
+        /// Update the geometry displayed on screen to be now conformed by the anchor calculated
+        faceGeometry.update(from: faceAnchor.geometry)
+
+        /// Update node transforms
+        leftEyeNode?.simdTransform = faceAnchor.leftEyeTransform
+        rightEyeNode?.simdTransform = faceAnchor.rightEyeTransform
+    }
+}
---
+++ ARContactLenses/Utilities/ImageNode.swift
@@ -0,0 +1,19 @@
+
+import SceneKit
+
+final class ImageNode: SCNNode {
+    init(width: CGFloat,
+         height: CGFloat,
+         image: UIImage) {
+
+        super.init()
+        let plane = SCNPlane(width: width, height: height)
+        plane.firstMaterial?.diffuse.contents = image
+        plane.firstMaterial?.isDoubleSided = true
+        geometry = plane
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
